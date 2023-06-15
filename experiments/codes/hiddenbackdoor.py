62407de7942abc0b5861b909ed44c0bf4a70551c
arm-on <malekzadeh@ieee.org>
2023-06-15 04:18:18 +0000

    name changed

---
+++ experiments/codes/hiddenbackdoor.py
@@ -0,0 +1,16 @@
+class Attack:
+
+    def __init__(self):
+        return
+
+    def run(self):
+        return
+
+
+class HiddenBackdoor(Attack):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
